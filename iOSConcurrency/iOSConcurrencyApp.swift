06a8f0ac7c68bc6cb94875df6f279f5f2e0dfc54
Daniel Sutcliffe <daniel.sutcliffe@co-operativebank.co.uk>
2025-07-30 07:42:03 +0100

    Setup Base SwiftUI

---
+++ iOSConcurrency/iOSConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSConcurrencyApp.swift
+//  iOSConcurrency
+//
+//  Created by Daniel Sutcliffe on 30/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct iOSConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
